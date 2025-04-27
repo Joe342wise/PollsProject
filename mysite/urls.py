83aab0d5b450663893207ac3da8dab156eae02dd
jow342wise <jnyarko342@gmail.com>
2025-04-27 02:15:06 +0000

    Add Django Debug Toolbar for enhanced debugging capabilities

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from debug_toolbar import urls as debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
